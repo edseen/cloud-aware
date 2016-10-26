2b97293b6446dfac657d9873a431da5d9287ec20
Edgardo Serna-Enciso <edseen@gmail.com>
2016-10-26 11:34:36 -0500

    Script para crear un volumen

---
+++ challenge04/create_volume.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+from shade import *
+
+simple_logging(debug=True)
+conn = openstack_cloud(cloud='myfavoriteopenstack')
+ 
+conn.create_volume(size=1, display_name='database', description='A database volume')
