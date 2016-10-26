1c27e9cdc403a309e76a32af532dbbe94fedf88b
Edgardo Serna-Enciso <edseen@gmail.com>
2016-10-26 11:55:52 -0500

    Configuracion de MySQL

--- challenge04/init.sh
+++ challenge04/init.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get -y install mysql-server
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+sudo sed -i -e "/bind-address/d" /etc/mysql/my.cnf
+sudo service mysql restart
+
+sudo mysqladmin password password
+sudo mysql -uroot -ppassword mysql -e "CREATE DATABASE IF NOT EXISTS edseenTest; GRANT ALL PRIVILEGES ON edseenTest.* TO 'edseenTest'@'%' IDENTIFIED BY 'password';"
+URL_DATABASE='mysql://root:password@localhost/edseenTest'
 
