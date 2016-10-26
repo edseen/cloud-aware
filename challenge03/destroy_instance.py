0157dee4438e802e9e951f6014ad7f0c47a659a0
Edgardo Serna-Enciso <edseen@gmail.com>
2016-10-25 19:57:56 -0500

    Challenge03: - delete_objects-container.py, ~ destroy_instance.py

--- challenge03/destroy_instance.py
+++ challenge03/destroy_instance.py
@@ -9,3 +9,12 @@ instance_name = 'pokedex'
 
 print "\nDeleting server:"
 conn.delete_server(name_or_id=instance_name)
+
+container_name = 'pokedex'
+
+print '\nDeleting objects in %s' % container_name
+for object in conn.list_objects(container_name):
+	print('Good bye %s!' % object['name'])
+	conn.delete_object(container_name, object['name'])
+
+conn.delete_container(container_name)
