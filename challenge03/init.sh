102e7d9b51038c1366e77fe2cebbcea9ecf5b5d1
Edgardo Serna-Enciso <edseen@gmail.com>
2016-10-26 09:55:23 -0500

    Challenge03: - create_instance.py, - destroy_instance.py, - index.html, - init.sh, - upload_images.py

--- challenge03/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-sudo apt-get -y install wget apache2
-
-wget https://raw.githubusercontent.com/edseen/cloud-aware/master/challenge03/index.html
-sudo cp index.html /var/www/html/
