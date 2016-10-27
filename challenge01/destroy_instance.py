dd0be2998e09a4a686fb9d064a8a5a8088fb79ab
Edgardo Serna-Enciso <edseen@gmail.com>
2016-10-27 18:57:05 -0500

    Eliminar instancia con webservice

--- challenge01/destroy_instance.py
+++ challenge01/destroy_instance.py
@@ -5,7 +5,7 @@ from shade import *
 simple_logging(debug=True)
 conn = openstack_cloud(cloud='myfavoriteopenstack')
  
-instance_name = 'edseen-01'
+instance_name = 'edseen'
 
 print "\nDeleting server:"
 conn.delete_server(name_or_id=instance_name)
