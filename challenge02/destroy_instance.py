98d4c2b83790b75a71cc592ea1aab91c4611fe66
Edgardo Serna-Enciso <edseen@gmail.com>
2016-10-27 18:34:45 -0500

    Eliminar instancia de Fisrt App

--- challenge02/destroy_instance.py
+++ challenge02/destroy_instance.py
@@ -5,7 +5,6 @@ from shade import *
 simple_logging(debug=True)
 conn = openstack_cloud(cloud='myfavoriteopenstack')
  
-#instance_name = 'testing'
 instance_name = 'all-in-one'
 
 conn.delete_server(name_or_id=instance_name)
